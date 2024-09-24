ef7313a72571b5e41859946fc4c80c1330e1f751
Lainey <misslenlen@gmail.com>
2024-09-24 09:14:31 -0700

    options in drop table migration spots

--- backend/db/migrations/20240924004515-create-spot.js
+++ backend/db/migrations/20240924004515-create-spot.js
@@ -80,6 +80,7 @@ module.exports = {
     );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Spots");
+    options.tableName = "Spots";
+    await queryInterface.dropTable(options);
   },
 };
