8d0c24f33ded3dcde7c142e8e2dfc750610471fd
Lainey <misslenlen@gmail.com>
2024-09-23 17:34:04 -0700

    added logic to only show stack trace in dev

--- backend/app.js
+++ backend/app.js
@@ -82,12 +82,22 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
-  res.json({
-    title: err.title || "Server Error",
-    message: err.message,
-    errors: err.errors,
-    stack: isProduction ? null : err.stack,
-  });
+
+  if (!isProduction) {
+    // enable cors only in development
+    res.json({
+      title: err.title || "Server Error",
+      message: err.message,
+      errors: err.errors,
+      stack: err.stack,
+    });
+  } else {
+    res.json({
+      title: err.title || "Server Error",
+      message: err.message,
+      errors: err.errors,
+    });
+  }
 });
 
 // ***** EXPORTS *****/
