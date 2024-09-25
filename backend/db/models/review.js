76ff6292664f66ee69c18cadfd7902622ac875ac
Lainey <misslenlen@gmail.com>
2024-09-24 23:19:18 -0700

    adding reviews seed table and data

---
+++ backend/db/models/review.js
@@ -0,0 +1,48 @@
+// backend/db/models/review.js
+
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    static associate(models) {
+      Review.belongsTo(models.Spot, {
+        foreignKey: "spotId",
+        as: "spot",
+      });
+
+      Review.belongsTo(models.User, {
+        foreignKey: "userId",
+        as: "user",
+      });
+    }
+  }
+  Review.init(
+    {
+      spotId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      review: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stars: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Review",
+    }
+  );
+  return Review;
+};
