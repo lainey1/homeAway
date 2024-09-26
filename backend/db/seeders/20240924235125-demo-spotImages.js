e25d67517f925364efff1e08076884d1b86d4d41
Lainey <misslenlen@gmail.com>
2024-09-26 11:03:48 -0700

    testing changes to spot-Images

--- backend/db/seeders/20240924235125-demo-spotImages.js
+++ backend/db/seeders/20240924235125-demo-spotImages.js
@@ -11,44 +11,46 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await SpotImage.bulkCreate([
-      {
-        spotId: 1,
-        url: "https://cdn.prod.website-files.com/5dcc7f8c449e597ed83356b8/5faae1191b673c881b077e1f_ogaa-min.png",
-        preview: true,
-      },
-      {
-        spotId: 1,
-        url: "https://www.computerworld.com/wp-content/uploads/2024/03/programming_coding_elements_lines_of_code_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056840_2400x1600-100795791-orig-100918079-orig.jpg",
-        preview: false,
-      },
-      {
-        spotId: 2,
-        url: "https://cdn.prod.website-files.com/5dcc7f8c449e597ed83356b8/5faae1191b673c881b077e1f_ogaa-min.png",
-        preview: true,
-      },
-      {
-        spotId: 2,
-        url: "https://www.computerworld.com/wp-content/uploads/2024/03/programming_coding_elements_lines_of_code_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056840_2400x1600-100795791-orig-100918079-orig.jpg",
-        preview: false,
-      },
-      {
-        spotId: 3,
-        url: "https://cdn.prod.website-files.com/5dcc7f8c449e597ed83356b8/5faae1191b673c881b077e1f_ogaa-min.png",
-        preview: true,
-      },
-      {
-        spotId: 3,
-        url: "https://www.computerworld.com/wp-content/uploads/2024/03/programming_coding_elements_lines_of_code_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056840_2400x1600-100795791-orig-100918079-orig.jpg",
-        preview: false,
-      },
-    ], options);
-   
+    await SpotImage.bulkCreate(
+      [
+        {
+          spotId: 1,
+          url: "https://cdn.prod.website-files.com/5dcc7f8c449e597ed83356b8/5faae1191b673c881b077e1f_ogaa-min.png",
+          preview: true,
+        },
+        {
+          spotId: 1,
+          url: "https://www.computerworld.com/wp-content/uploads/2024/03/programming_coding_elements_lines_of_code_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056840_2400x1600-100795791-orig-100918079-orig.jpg",
+          preview: false,
+        },
+        {
+          spotId: 2,
+          url: "https://cdn.prod.website-files.com/5dcc7f8c449e597ed83356b8/5faae1191b673c881b077e1f_ogaa-min.png",
+          preview: true,
+        },
+        {
+          spotId: 2,
+          url: "https://www.computerworld.com/wp-content/uploads/2024/03/programming_coding_elements_lines_of_code_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056840_2400x1600-100795791-orig-100918079-orig.jpg",
+          preview: false,
+        },
+        {
+          spotId: 3,
+          url: "https://cdn.prod.website-files.com/5dcc7f8c449e597ed83356b8/5faae1191b673c881b077e1f_ogaa-min.png",
+          preview: true,
+        },
+        {
+          spotId: 3,
+          url: "https://www.computerworld.com/wp-content/uploads/2024/03/programming_coding_elements_lines_of_code_development_developers_teamwork_by_dean_mitchell_gettyimages-1055056840_2400x1600-100795791-orig-100918079-orig.jpg",
+          preview: false,
+        },
+      ],
+      options
+    );
   },
 
   async down(queryInterface, Sequelize) {
     options.tableName = "SpotImages";
     // await queryInterface.dropTable(options);
-    return queryInterface.bulkDelete(options, null, {});  
+    return queryInterface.bulkDelete(options, null, {});
   },
 };
