721773a98e4f2c4af1492301f01099003f9b2a51
Lainey <misslenlen@gmail.com>
2024-09-27 11:25:47 -0700

    mocha test bulk fixes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -38,7 +38,7 @@ router.post("/", validateLogin, async (req, res, next) => {
   });
 
   if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-    const err = new Error("Login failed");
+    const err = new Error("Invalid credentials");
     err.status = 401;
     err.title = "Invalid credentials";
     err.errors = { credential: "The provided credentials were invalid." };
